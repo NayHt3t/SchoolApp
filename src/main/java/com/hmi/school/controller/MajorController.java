3879e0f39b179f50b179e3113f83fd7e7c86a93b
nayhtet <nayhtet.nyeinchan.2199@gmail.com>
2024-02-27 12:59:34 +0630

    added new major form

--- src/main/java/com/hmi/school/controller/MajorController.java
+++ src/main/java/com/hmi/school/controller/MajorController.java
@@ -1,14 +1,38 @@
 package com.hmi.school.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hmi.school.entity.Major;
+import com.hmi.school.service.MajorService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class MajorController {
+	
+	private final MajorService majorService;
+
+	public MajorController(MajorService majorService) {
+		super();
+		this.majorService = majorService;
+	}
 
 	@GetMapping("/all")
-	public String all() {
+	public String all(Model model) {
+		List<Major> majorList = majorService.getAllMajors();
+		model.addAttribute("majorlist", majorList);
 		return "major-list";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "add-major.html";
+	}
+	
 
 }
