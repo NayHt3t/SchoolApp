ad51b3d800fbf9dabb5dc6a71a9b513ebab793ab
nayhtet <nayhtet.nyeinchan.2199@gmail.com>
2024-02-28 12:17:11 +0630

    28/2/2024 first half

--- src/main/java/com/hmi/school/controller/MajorController.java
+++ src/main/java/com/hmi/school/controller/MajorController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.hmi.school.entity.Major;
 import com.hmi.school.service.MajorService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -30,9 +30,18 @@ public class MajorController {
 	}
 	
 	@GetMapping("/create")
-	public String create() {
-		return "add-major.html";
+	public String create(Model model) {
+		model.addAttribute("major", new Major());
+		return "add-major";
 	}
+	
+	@PostMapping("/create")
+	public String postMajor(@ModelAttribute Major major) {
+		Major createdMajor = majorService.saveMajor(major);
+		System.out.println("created major id = " + createdMajor.getId());
+		return "redirect:/all";
+	}
+	
 	
 
 }
